f2f2d3d7d65d98cf56bbd56b31b118099d732d13
kratos <lin790292154@163.com>
2017-11-26 18:01:46 +0800

    add

--- day3/homework_shoping.py
+++ day3/homework_shoping.py
@@ -1,3 +1,6 @@
+#write by lcj
+
+
 '''
 购物车
     用户入口：
@@ -10,3 +13,30 @@
 可以写成两个脚本，一个用户脚本。一个商家脚本。
 
 '''
+
+
+with open('shop.txt','r',) as file1:
+    salary = file1.readline()
+    a = file1.read()
+    shop_list = a.splitlines()
+    i=0
+    shop_dict={}
+    while i < len(shop_list)-2:
+        shop_dict[shop_list[i]]=shop_list[i+1]
+        i+=2
+    print(salary)
+
+if int(salary) == 0:
+    salary = int(input("welcome to shop first,please input your salary: "))
+
+while True:
+    print(shop_dict)
+    choice = input("input goods name:")
+    if choice in shop_dict:
+        c = shop_dict[choice]
+        salary = salary - int(shop_dict[choice])
+        print("shopping success ,you have salary now:",salary)
+
+    else:
+        print("my shop has's it :",choice)
+
